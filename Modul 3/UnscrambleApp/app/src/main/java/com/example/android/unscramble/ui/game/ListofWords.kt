e3333d0e6f2df3dc0ea1cf52f0eb3ff0a52375cc
farlyhaydydjalil <farlyhaydy123@gmail.com>
2024-06-04 14:20:15 +0800

    commit

---
+++ Modul 3/UnscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,36 @@
+package com.example.android.unscramble.ui.game
+
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// List with all the words for the Game
+val allWordsList: List<String> =
+    listOf("apel", "angin", "anak", "asap", "asri", "awan", "ayam", "ajar", "amal", "adab",
+        "air", "aku", "ayam", "api", "aduh", "anjing", "antar", "arah", "abadi", "alun",
+        "buku", "bola", "ban", "batu", "baik", "buah", "baju", "bangku", "bunga", "belajar",
+        "bambu", "besar", "belut", "berita", "burung", "cinta", "cawan", "cari", "cahaya", "cat",
+        "cuka", "cacing", "ceri", "celana", "cerah", "celana", "cabang", "cetak", "cerdas", "cukur",
+        "daun", "dasi", "daging", "dapur", "dada", "dekat", "diri", "domba", "duduk", "darat",
+        "durian", "dalam", "damai", "danau", "dosen", "elang", "empat", "es", "emas", "enak",
+        "empat", "elok", "ekor", "epal", "embun", "fajar", "film", "foto", "fasilitas", "faktor",
+        "fiksi", "final", "firasat", "fantasi", "flora", "furnitur", "firman", "fisika", "formal", "fitur",
+        "gelas", "garam", "gula", "gajah", "gunung", "gembira", "guntur", "gabus", "gadis", "gajah",
+        "gagang", "gantung", "gantung", "gas", "gerak", "hujan", "hutan", "hari", "hati", "hewan",
+        "hidup", "harum", "hotel", "hitam", "halaman", "ikan", "indah", "ilmu", "istana", "inspirasi",
+        "jalan", "jendela", "jam", "jatuh", "jari", "jambu", "jaring", "jernih", "jeruk", "jawaban",
+        "kucing", "kuda", "kursi", "kertas", "kapal", "kota", "kaki", "kamar", "kasur",
+        "kayak", "langit", "laut", "lilin", "lembut", "lentera", "lintas", "layar", "lebar", "lembah", "lampu",
+        "matahari", "meja", "makan", "minum", "manis","mimpi", "mobil", "muda", "mudah", "musik",
+        "nasi", "nyamuk", "nangka", "naik", "nyata", "nyanyi", "nona", "novel", "namun", "nara",
+        "ombak", "oren", "obyek", "oleh", "otak", "otot", "ongkos", "opini", "olah", "obat",
+        "pohon", "pisau", "pantai", "padi", "pagar", "payung", "piring", "pulau", "pusat", "panas",
+        "papan", "pintu", "pedas", "pelangi", "pena", "quran", "quadrat", "quark", "quasar", "quiz",
+        "robot", "rumah", "rusa", "roda", "rapi", "rak", "rawat", "rasa", "rakit", "rambut",
+        "sawah", "sapi", "sinar", "susu", "sawah", "sepeda", "surat", "sore", "sopan", "sarung",
+        "taman", "tanah", "tanda", "tangan", "tali", "tahu", "takut", "tas", "tungku", "topi",
+        "ular", "udara", "ubi", "ulang", "umur", "upah", "udang", "usaha", "utama", "uang",
+        "vaksin", "vila", "vokal", "valuta", "vital", "venus", "varian", "virus", "vitamin", "volume",
+        "waktu", "warna", "wajah", "waktu", "wangi", "warung", "wadah", "wawasan", "wacana", "waspada",
+        "xilofon", "xenon", "xilografi", "xerofit", "xenofobia", "yoghurt", "yak", "yodium", "yoyo", "yoga",
+        "yakin", "yuk", "yatim", "yul", "yumpie", "zebra", "zaman", "zakat", "zodiak", "zona")
