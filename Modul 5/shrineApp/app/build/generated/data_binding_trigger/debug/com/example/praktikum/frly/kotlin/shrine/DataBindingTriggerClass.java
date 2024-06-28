19793629a229aa599d43018bcf2513a607a51507
farlyhaydyhdjalil <farlyhaydy123@gmail.com>
2024-06-29 00:58:50 +0800

    commit

--- Modul 5/shrineApp/app/build/generated/data_binding_trigger/debug/com/example/praktikum/frly/kotlin/shrine/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.praktikum.frly.kotlin.shrine;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- Modul 5/shrineApp/app/build/generated/source/kapt/debug/com/example/praktikum/frly/kotlin/shrine/BR.java
+++
@@ -1,5 +0,0 @@
-package com.example.praktikum.frly.kotlin.shrine;
-
-public class BR {
-  public static final int _all = 0;
-}
